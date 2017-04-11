330b94cca5e3561da48ca8aa0dc6ff9d9227376b
zachstecher <malleablemedia@gmail.com>
2017-04-11 11:21:45 -0400

    Add files via upload

---
+++ AS.js
@@ -0,0 +1,208 @@
+
+
+'use strict';
+
+function AlexaSkill(appID)
+{
+this._appID = appID;
+}
+
+AlexaSkill.speechOutputType = {
+PLAIN_TEXT: 'PlainText',
+SSML:'SSML'
+}
+
+AlexaSkill.prototype.requestHandlers = {
+LAunchRequest:function(event,content,response)
+{
+this.eventHandlers.onLaunch.call(this, event.request, event.session, response);
+},
+IntentRequest: function(event,context,response)
+{
+this.eventHandlers.onIntend.call(this,event.request, event.session, response);
+},
+SessionEndedRequest: function (event, context)
+{
+this.eventHandlers.onSessionEnded(event.request, venet.session);
+context.succeed();
+}
+};
+
+
+AlexaSkill.prototype.eventHandlers = {
+
+onSessionStarted: function (sessionStartedRequest, session)
+{
+},
+
+onLaunch: fuction(LaunchRequest, session, response)
+
+{
+},
+
+
+
+onLaunch: fuction(LaunchRequest, session, response)
+{
+throw"onLaunch should be overriden by subclasss";
+}'
+
+onIntent:function(intentRequest,session,response)
+{
+var intent = intentRequest.intent,
+intentName = intentRequest.intent.name,
+intentHandler = this.intentHandlers[intentName];
+if(intentHandler)
+{
+console.log('dispatch intent='+intentName);
+intentHandler.call(this, intent, session, response);
+}
+else
+{
+throw 'Unsupported intent = ' + intentName;
+}
+},
+
+onSessionEnded: function(sessionEndedRequest, session)
+{
+}
+};
+
+AlexaSkill.prototype.intentHandlers = {};
+AlexaSkill.prototype.execute = function(event,context)
+{
+try
+{
+console.log("Session applicationID:"+event.session.application.applucationID);
+
+if(this._appID&&event.session.application.applicationID!==this._appID)
+{
+console.log("The apllicationIDs dont match:"+event.session.application.applicationID+this,_appId);
+throw "Invalid applicationID";
+}
+if(!event.session.attributes)
+{
+event.session.attributes ={};
+}
+if(event.session.new)
+{
+this.eventHandlers.onSessionStarted(event.request, event.session);
+}
+var requestHandler=this.requestHandlers[event.request.type];
+requestHandler.call(this,event,context, new Response(context,event.session));
+}
+catch(e)
+{
+console.log("Unexpected exception"+e);
+context.fail(e);
+}
+};
+var Respose = function(context,session)
+{
+this._context = context;
+this._session = session;
+};
+
+function createSpeechObject(optionParam)
+{
+if(optionsParam && optionsParam.type ***'SSML')
+{
+return
+{
+type: optionsParam.type,
+ssml: optionsParam.speech
+};
+}else
+{
+return{
+type: optionsParam.type ||"PlainText',
+text: optionsParam.speech || optionsParam
+}
+}
+}
+
+
+Response.prototype = (fuction()
+{
+
+var buildSpeechletResponse = fuction (options)
+{
+var alexaResponse = {
+outputSpeech: createSpeechObject(options.output),
+shouldEndSession: options.shouldEndSession
+};
+
+if(options.reprompt)
+{
+alexaResponse.reprompt = {
+outputSpeech: createSpeechObject(options.output)
+};
+}
+if(options.cardTitle && options.cardContent)
+{
+alexaResponse.card = { 
+type: "Simple",
+title: options.cardTitle,
+content: options.cardContent
+};
+}
+
+var returnResult = {
+version: '1.0',
+response: alexaRsponse
+};
+
+if(options.session && options.session.attributes)
+{
+returnReult.sessionAttributes = options.session.attributes;
+}
+return returnResult;
+};
+
+return
+{
+tell:function(speechOutput)
+{
+this._context.succeed(buildSpeechletResponse({
+session:this._session,
+output:speechOutput,
+shouldEndSession:true
+}));
+},
+ask:fuction(speechOutput,repromptSpeech)
+{
+this._context.succeed(buildSpeechletResponse({
+session:this._session,
+output: speechOutput,
+reprompt: repromptSeech,
+shouldEndSession:false
+}));
+},
+
+askWithCard: fuction(speechOutput,repromptSpeech, cardTitle, cardContent)
+{
+this._context.succeed(buildSpeechletResponse({
+session:this._session,
+output:speechOutput,
+reprompt:repromptSpeech,
+cardTitle:cardTitle,
+cardContent: cardContent,
+shouldEndSession: false
+}
+));
+}
+};
+}
+)
+();
+module.exports = AlexaSkill;
+
+
+
+
+
+
+
+
+
+
---
+++ ind.js
@@ -0,0 +1,131 @@
+var APP_ID = undefined;
+
+//Env Config
+
+var config = {};
+config.IOT_BROKER_ENDPOINT = "a1ftttk3ucqy90.iot.us-east-1.amazonaws.com".toLowerCase();
+config.IOT_BROKER_REGION = "us-east-1";
+config.IOT_THING_NAME = "wago";
+
+// Loading AWS SDK libraries
+
+var ANS = require('aws-sdk');
+AWS.config.region = config.IOT_BROKER_REGION;
+//Intializing client for IOT
+var iotData = new AWS.IotData({endpoint:config.IOT_BROKER_ENDPOINT});
+var AlexaSkill = require('./AlexaSkill');
+var HelloWorld = function ()
+{
+AlexaSkill.call(this, APP_ID);
+};
+
+//Extend AlexaSkill
+HelloWorld.prototype = Object.create(AlexaSkill.prototype);
+HelloWorld.prototype.constructor = HelloWorld;
+
+HelloWorld.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session)
+{
+console.log("HelloWorld onSessionStarted requestID:" + sessionStartedRequest.requestID + ".sessionID: " + session.sessionID);
+
+// any kind of initialization 
+
+HelloWorld.prototype.eventHandlers.onLaunch = function (launchRequest,session,response)
+{
+console.log("HelloWorld onLaunch requestID: " + launchRequest.requestID +",sessionID:"+session.sessionID);
+var speechOutput = "Welcome to the Alexa skills, say hello";
+var repromptText = "You can say Hello";
+response.ask(speechOutput,repromptText);
+// logic cleanup 
+};
+
+HelloWorld.prototype.intendHandlers = {
+
+// register custom intend handlers
+"RaspberryControlOnIntent": function(intent, session,response)
+{
+
+// response.tellWithCard("Hello World!","Hello World");
+console.log("FB started");
+
+var repromptText = null;
+var sessionAttributes = {};
+var shouldEndSession = true;
+var speechOutput = "";
+var payloadObj=1; //On
+//preparing parameters of the update call
+
+var paramsUpdate = {
+topic:"/wago",
+payload: JSON.strimgify(payloadObj),
+qos:0
+};
+iotData.publish(paramsUpdate, function(err, data)
+{
+if (err)
+{
+//Handle the error here
+
+console.log(MQTT Error" + data);
+}
+else
+{
+speechOutput = "Turning Raspberry on now.";
+console.log(data);
+response.tell(speechOutput);
+//callback(sessionAttributes,buildSpeechletRespnse(intent.name,speechOutput,repromptText)
+}
+});
+},
+
+
+
+
+"RaspberryControlOffIntent": function(intent,session,response)
+{
+console.log("FB started");
+var repromptText = null;
+var sessionAttrivutes = {};
+var shouldEndSession = true;
+var speechOutput = "";
+
+//Setting the Raspberry to 0 for activating the device
+var pyloadObj=0;     //Off
+var paramsUpdate = {
+topic:"/wago",
+payload: JSON.strimgify(payloadObj),
+qos:0
+};
+iotData.publish(paramsUpdate, function(err, data)
+{
+if (err)
+{
+//Handle the error here
+
+console.log(MQTT Error" + data);
+}
+else
+{
+speechOutput = "Turning Raspberry off now.";
+console.log(data);
+//callback(sessionAttributes,buildSpeechletRespnse(intent.name,speechOutput,repromptText)
+}
+});
+},
+
+"AMAZON.HelpIntent": function (intend,session,response)
+{
+response.ask("You can ask me to turn on or off", "You can ask me to turn on or off")
+}
+};
+
+//Creating handler that reponds to Alexa's Reequest
+exports.handler = function (event, context)
+{
+
+// creating an instance of the HelloWorld Skill.
+
+var helloWorld = new HelloWorld();
+helloWorld.execute(event,context);
+};
+
+
